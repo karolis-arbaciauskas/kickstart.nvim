1d8cdbcfba2413bc240d9b9c8034b32ea623153c
Karolis Arbaciauskas <karolis.arbaciauskas@gmail.com>
2025-11-16 17:24:58 +0200

    Add keymap

--- init.lua
+++ init.lua
@@ -799,6 +799,7 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        ['<M-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
