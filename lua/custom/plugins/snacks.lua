591c708497e41f78b283ca8e47659013ea2d2b2b
Karolis Arbaciauskas <karolis.arbaciauskas@gmail.com>
2025-11-16 17:05:29 +0200

    Replace telescope with picker / enable folding

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -7,39 +7,149 @@ return {
   ---@diagnostic disable-next-line: undefined-doc-name
   ---@type snacks.Config
   opts = {
+    picker = {},
     lazygit = {},
     gitbrowse = {},
   },
   keys = {
+    -- Git operations
     {
       '<leader>gg',
       function()
         Snacks.lazygit { cwd = vim.fn.getcwd() }
       end,
-      { desc = 'Lazygit (Root Dir)' },
+      desc = 'Lazygit (Root Dir)',
     },
     {
       '<leader>gf',
       function()
         Snacks.lazygit.log_file()
       end,
-      { desc = 'Lazygit Current File History' },
+      desc = 'Lazygit Current File History',
     },
     {
       '<leader>gl',
       function()
         Snacks.lazygit.log { cwd = vim.fn.getcwd() }
       end,
-      { desc = 'Lazygit Log' },
+      desc = 'Lazygit Log',
     },
     { '<leader>gb', '<cmd>Gitsigns blame<cr>', desc = 'Git blame' },
-    { '<leader>gs', '<cmd>Telescope git_status<CR>', desc = 'Git Status' },
+    {
+      '<leader>gs',
+      function()
+        Snacks.picker.git_status()
+      end,
+      desc = 'Git Status',
+    },
     {
       '<leader>go',
       function()
         Snacks.gitbrowse()
       end,
       desc = 'Git Browse',
+    },
+
+    -- Picker: Search operations
+    {
+      '<leader>sh',
+      function()
+        Snacks.picker.help()
+      end,
+      desc = '[S]earch [H]elp',
+    },
+    {
+      '<leader>sk',
+      function()
+        Snacks.picker.keymaps()
+      end,
+      desc = '[S]earch [K]eymaps',
+    },
+    {
+      '<leader>sf',
+      function()
+        Snacks.picker.files()
+      end,
+      desc = '[S]earch [F]iles',
+    },
+    {
+      '<leader>ss',
+      function()
+        Snacks.picker()
+      end,
+      desc = '[S]earch [S]elect Picker',
+    },
+    {
+      '<leader>sw',
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = '[S]earch current [W]ord',
+    },
+    {
+      '<leader>sg',
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = '[S]earch by [G]rep',
+    },
+    {
+      '<leader>sd',
+      function()
+        Snacks.picker.diagnostics()
+      end,
+      desc = '[S]earch [D]iagnostics',
+    },
+    {
+      '<leader>sr',
+      function()
+        Snacks.picker.resume()
+      end,
+      desc = '[S]earch [R]esume',
+    },
+    {
+      '<leader>s.',
+      function()
+        Snacks.picker.recent()
+      end,
+      desc = '[S]earch Recent Files ("." for repeat)',
+    },
+    {
+      '<leader><leader>',
+      function()
+        Snacks.picker.buffers {
+          win = {
+            input = {
+              keys = {
+                ['d'] = 'bufdelete',
+              },
+            },
+            list = { keys = { ['d'] = 'bufdelete' } },
+          },
+        }
+      end,
+      desc = '[ ] Find existing buffers',
+    },
+    {
+      '<leader>/',
+      function()
+        Snacks.picker.lines()
+      end,
+      desc = '[/] Search in current buffer',
+    },
+    {
+      '<leader>s/',
+      function()
+        Snacks.picker.grep_buffers()
+      end,
+      desc = '[S]earch [/] in Open Files',
+    },
+    {
+      '<leader>sn',
+      function()
+        Snacks.picker.files { cwd = vim.fn.stdpath 'config' }
+      end,
+      desc = '[S]earch [N]eovim files',
     },
   },
 }
