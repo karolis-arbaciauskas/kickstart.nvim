78a4344b64a25baa2fdd3a14a2db1acd7800d77a
Karolis Arbaciauskas <karolis.arbaciauskas@gmail.com>
2025-11-16 18:25:09 +0200

    Use blink ident lines

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,9 +1,25 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
+  -- { -- Add indentation guides even on blank lines
+  --   'lukas-reineke/indent-blankline.nvim',
+  --   -- Enable `lukas-reineke/indent-blankline.nvim`
+  --   -- See `:help ibl`
+  --   main = 'ibl',
+  --   opts = {},
+  -- },
+  {
+    'saghen/blink.indent',
+    --- @module 'blink.indent'
+    --- @type blink.indent.Config
+    opts = {
+      static = {
+        highlights = { 'BlinkIndent' },
+      },
+      scope = {
+        highlights = { 'BlinkIndentScope' },
+        underline = {
+          highlights = { 'BlinkIndentScopeUnderline' },
+        },
+      },
+    },
   },
 }
